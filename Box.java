a1d2cad921fc9f492f1eabd9c7e3235c416f29b4
Bart Massey <bart@cs.pdx.edu>
2013-05-21 19:28:29 -0700

    did numerous bug fixes and cleanups; now more efficient and prints in increasing order

--- Box.java
+++ Box.java
@@ -12,40 +12,34 @@ import java.util.*;
 public class Box {
     static final int allDigits = 0x1ff;
 
-    static HashSet<Integer> nexts(int digits, int roll) {
-        HashSet<Integer> result = new HashSet<Integer>();
-        for (int i = 1; i <= 9; i++) {
-            int count = appendNexts(result, digits, roll, 0, i);
-            if (i > 1 && count == 0)
-                break;
-        }
-        return result;
-    }
-
     static int bit(int v) {
         return 1 << (v - 1);
     }
 
-    static int appendNexts(HashSet<Integer> a, int digits, int roll,
-                    int cur, int len) {
+    static ArrayList<Integer> nexts(int digits, int roll) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 1; i <= 9; i++)
+            appendNexts(result, digits, roll, 0, i, 1);
+        return result;
+    }
+
+    static void appendNexts(ArrayList<Integer> a, int digits, int roll,
+                            int cur, int len, int start) {
         if (len == 1) {
-            if (((digits & ~cur) & bit(roll)) > 0) {
+            if (roll >= start && (digits & ~cur & bit(roll)) > 0)
                 a.add(cur | bit(roll));
-                return 1;
-            }
-            return 0;
+            return;
         }
-        int t = 0;
-        for (int i = 1; i <= 9; i++) {
+        for (int i = start; i < 9; i++) {
             if ((digits & ~cur & bit(i)) == 0)
                 continue;
-            t += appendNexts(a, digits, roll - i, cur | bit(i), len - 1);
+            appendNexts(a, digits,
+                        roll - i, cur | bit(i), len - 1, i + 1);
         }
-        return t;
     }
 
     static void printChoice(int c) {
-        for (int i = 9; i >= 1; --i)
+        for (int i = 1; i <= 9; i++)
             if ((c & bit(i)) > 0)
                 System.out.print(i);
         System.out.println();
@@ -59,7 +53,7 @@ public class Box {
             int d = digitStr.charAt(i) - '0';
             digits |= bit(d);
         }
-        HashSet<Integer> choices = nexts(digits, roll);
+        ArrayList<Integer> choices = nexts(digits, roll);
         for (int c : choices)
             printChoice(c);
     }
